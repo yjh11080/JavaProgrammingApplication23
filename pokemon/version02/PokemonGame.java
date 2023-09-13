bd5c367ffbb942f857b64e159cab7d7474c72472
yjh11080 <yjh11080@g.shingu.ac.kr>
2023-09-13 14:22:08 +0900

    week03 v01

---
+++ pokemon/version02/PokemonGame.java
@@ -0,0 +1,22 @@
+package version02;
+
+public class PokemonGame {
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon();
+        Pokemon charizard = new Pokemon();
+
+        pikachu.name = "피카츄";
+        charizard.name = "리자몽";
+
+        pikachu.level = 3;
+        pikachu.hp = 100;
+
+        charizard.level = 36;
+        charizard.hp = 800;
+
+        System.out.println(charizard.name + "의 체력은 " + charizard.hp + "이고 레벨은 " + charizard.level + "입니다");
+        charizard.evolve(charizard);
+        pikachu.evolve(pikachu);
+        pikachu.evolve(pikachu);
+    }
+}
