63eb6fb1e62b5219e179ce32ef2566e2a970df3d
yjh11080 <yjh11080@g.shingu.ac.kr>
2023-09-20 14:04:47 +0900

    week04 v17

--- week04/src/PokemonGame.java
+++ week04/src/PokemonGame.java
@@ -2,6 +2,7 @@
 public class PokemonGame {
     public static void main(String[] args) {
          Pikachu pikachu = new Pikachu();
+        pikachu.setFlyable(new NoFly());  // 프로그램 실행 중 동적으로 하늘을 나는 방식을 변경
          pikachu.performFly();
     }
 }
