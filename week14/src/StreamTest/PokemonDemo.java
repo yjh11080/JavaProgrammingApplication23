1168f7a779afa270115cf3a56309146df5c529a7
yjh11080 <yjh11080@g.shingu.ac.kr>
2023-11-29 16:23:22 +0900

    week14 v58

--- week14/src/StreamTest/PokemonDemo.java
+++ week14/src/StreamTest/PokemonDemo.java
@@ -1,31 +1,30 @@
 package StreamTest;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-class Pokemon{
-
+abstract class Pokemon{
     protected int level;
-    protected  String name;
+    protected String name;
 
     public Pokemon(int level, String name) {
         this.level = level;
         this.name = name;
     }
-    //public  abstract void evolve();
+    public abstract void evolve();
 }
 
-
-class Pickachu extends Pokemon {
+class Pickachu extends Pokemon{
     public Pickachu(int level, String name) {
-        super(level,name);
+        super(level, name);
     }
+
+    @Override
     public void evolve() {
-        if(this.level >= 16) {
+        if(this.level >= 16){
             this.name = "Raichu";
-            System.out.println("축하합니다. 당신의 피카츄가 라이츄로 진화합니다.");
+            System.out.println("축하합니다. 당신의 피카츄가 라이츄로 진화합니다");
         } else {
             System.out.println("피카츄의 레벨이 부족합니다.");
         }
@@ -36,12 +35,12 @@ class Pickachu extends Pokemon {
 public class PokemonDemo {
     public static void main(String[] args) {
         List<Pickachu> pokemons = Stream.of(
-                new Pickachu(15,"피카1"),
-                new Pickachu(16,"피카2"),
-                new Pickachu(17,"피카3")
+                new Pickachu(15, "피카1"),
+                new Pickachu(16, "피카2"),
+                new Pickachu(17, "피카3")
         ).collect(Collectors.toList());
 
-        //pokemons.forEach(Pickachu :: evolve);
+        //pokemons.forEach(Pickachu::evolve);
         pokemons.forEach(l -> l.evolve());
     }
 }
